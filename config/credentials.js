40314f167cbf9d99e9d5f6e1dba1dfe9769ef385
muhammad jaysy <muhammad.jaysy.tif17@polban.ac.id>
2020-07-28 15:36:32 +0700

    modified cloud host

--- config/credentials.js
+++ config/credentials.js
@@ -1,6 +1,6 @@
 const credentials = {
     apiLocalHost: 'localhost:3000',
-    apiCloudHost: 'https://albid-api.azurewebsites.net:3000',
+    apiCloudHost: 'https://albid-api.azurewebsites.net',
     apiKey: 'APIKEYUNTUKAPLIKASILAYANANKEBIDANANKOTA211D3TIF2017POLBAN',
     apiRoute: {
         root: '/api',
